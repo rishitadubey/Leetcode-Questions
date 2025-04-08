72acf04fadc5873d1fbe967d7ea2b1e5710a79cc
rishita dubey <139675955+rishitadubey@users.noreply.github.com>
2025-04-08 12:05:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (53.06%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        vector<int>ans;
-                for(int i=1 ; i<=n ;i++){
-        if(n%i==0){
-            ans.push_back(i);
+        vector<int>ans;// make an ans array of alll factors
+                for(int i=1 ; i<=n ;i++){//traverse ans array from 1 to n
+        if(n%i==0){// if i is a factor of n
+            ans.push_back(i);//push back i in our ans array of fsctors
         }
         
-       if(ans.size()==k){
-            return ans[k-1];
+       if(ans.size()>=k){// ans arr is big enough for k,
+            return ans[k-1];//  since our ans arr indexing starts w 0 so we print out the kth -1 index of thye areray
         }
         }
     return -1;
