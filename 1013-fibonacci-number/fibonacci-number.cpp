e17a8328ac7baaf4e574b88cf22c91d26245b9c3
rishita dubey <139675955+rishitadubey@users.noreply.github.com>
2025-04-12 13:06:58 +0530

    Time: 12 ms (31.82%) | Memory: 7.7 MB (87.47%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n==0) return 0;
-    if(n==2 || n==1)  return 1;//bhai fuck u rishita
-           
-   return fib(n-1)+fib(n-2);//kaam
-     
-    
-   
+        if (n==0) return 0;             //base case 
+            if(n==2 || n==1)  return 1;
+                 return fib(n-1)+fib(n-2);//kaam 
+      
     }
 };
