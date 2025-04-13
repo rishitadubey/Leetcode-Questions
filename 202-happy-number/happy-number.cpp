b967c50a251417333c8b971a6a5d5e64bcf12469
rishita dubey <139675955+rishitadubey@users.noreply.github.com>
2025-04-13 21:33:54 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (62.76%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isHappy(int n) {
-         while(n>9){
-            int sum =0;
-            while(n){
-                 sum = sum + (n%10) *(n%10);
-                     n/= 10;
+         while(n>9){ // for   two and three digit numbers 
+            int sum =0;// intialize sum as 0
+            while(n>0){// postive h
+                 sum = sum + (n%10) *(n%10);//sum me 10 se divide krke digit ka last no. milk jata h 
+                     n/= 10; 
                  }
                   n=sum;
             }
