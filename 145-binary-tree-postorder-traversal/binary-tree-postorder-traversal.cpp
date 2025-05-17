239f1d6770d0b08a84179c694177415532063d1c
rishita dubey <139675955+rishitadubey@users.noreply.github.com>
2025-05-17 11:35:47 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (44.15%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   void postorderHelper( vector <int>& ans,TreeNode* root) {
+        if(root==NULL) {return ;}
+        postorderHelper(ans,root->left);
+         postorderHelper(ans,root->right);
+          ans.push_back(root->val);
+          return;
+    }
+     vector<int> postorderTraversal(TreeNode* root){
+        vector<int> ans;
+         postorderHelper(ans, root);
+         return ans;
+     }
+        
+    
+};
