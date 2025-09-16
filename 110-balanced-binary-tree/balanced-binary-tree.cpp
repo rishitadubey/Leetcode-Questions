25aacb9cf040abe2e340047af3ad3fa475beb72c
rishita dubey <139675955+rishitadubey@users.noreply.github.com>
2025-09-16 23:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (56.62%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -12,21 +12,22 @@
 class Solution {
 public:
  bool isBalanced(TreeNode* root) {
-        return checkHeight(root) != -1;
+        return checkHeight(root) != -1;// Public function to call the optimized checker
+
     }
-    int checkHeight(TreeNode* root) {
-        if(root==NULL)
+    int checkHeight(TreeNode* root) {// checkHeight function that returns height if balanced, -1 otherwise
+        if(root==NULL) //: Check if the root is null. If so, return true as an empty tree is balanced.
         {
             return true;
         }
           int leftHeight = checkHeight(root->left);
-          if(leftHeight == -1) return -1;
+          if(leftHeight == -1) return -1;// Left subtree not balanced
 
           int rightHeight = checkHeight(root->right);
-          if(rightHeight==-1) return -1;
+          if(rightHeight==-1) return -1;// Right subtree not balanced
 
-          if(abs(leftHeight-rightHeight)>1) return -1;
-          return max(leftHeight,rightHeight) +1;
+          if(abs(leftHeight-rightHeight)>1) return -1;// Current node unbalanced
+          return max(leftHeight,rightHeight) +1;// Return height if balanced
     }
     
 };
